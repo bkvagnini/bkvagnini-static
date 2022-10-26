5f72bf0489b6bcd2bf791488d52f10d7876ede4f
Brian K. Vagnini <brian@vagnini.net>
2022-10-25 23:16:54 -0400

    Initial commit.

---
+++ js/footer.js
@@ -0,0 +1,2 @@
+document.getElementById('footer')
+    .innerHTML = "<br /><small>&copy; 2022 Brian K. Vagnini</small>";
---
+++ js/header.js
@@ -0,0 +1,23 @@
+document.getElementById('header')
+    .innerHTML = `<h1 class = "hedder">bkvagnini portfolio</h1>
+    <section class="modal hidden">
+            <div class="flex">
+              <img src="https://avatars.githubusercontent.com/u/2326127?s=40&v=4" width="50px" height="50px" alt="Github user avatar" />
+              <button class="btn-close">⨉</button>
+            </div>
+            <div>
+              <h3>Stay in touch</h3>
+              <p><a href ="mailto:bkvagnini@gmail.com">Email: bkvagnini@gmail.com</a></p>
+              <p> Text: 850-583-0842</p>
+              <p> Call: 850-583-0842</p>
+            </div>
+          </section>
+          
+          <div class="overlay hidden"></div>
+          <button class="btn btn-open">Contact</button>`;
+
+    
+//https://stackoverflow.com/questions/30148661/innerhtml-to-execute-multiple-lines
+//use backticks for .innerHTML = "" to have multiple lines of HTML 
+
+//<a href =\"mailto:bkvagnini@gmail.com\">Contact Me</a></button></div>";
